d3b97a0daa9eeabd70f2245129a1ddbf740f9436
Diana <180865641+dgonzal7@users.noreply.github.com>
2025-03-18 21:05:20 -0400

    lesson 1 changes

--- assignment1/assignment1.py
+++ assignment1/assignment1.py
@@ -1,1 +1,122 @@
-# Write your code here.
+# Write your code here. Task 1 Hello
+def hey ():
+  print("Hello!")
+
+hey()
+
+# Task 2
+
+def greet (name):
+  print (f"Hello, {name}!")
+
+greet("Diana")
+
+# Task 3
+
+def calculator (x, y, z="multiply"):
+  
+  if z = "multiply":
+    return x * y
+  elif z = "add":
+    return x + y
+  elif z = "subtract":
+    return x - y
+  elif z = "divide":
+    return x / y
+  elif z = "modulo":
+    return x % y
+  elif z = "int_divide":
+    return x // y
+  elif z = "power":
+    return pow(x,y)
+  else print("You can't divide by 0!")
+
+calculator ()
+
+# Task 4
+
+def data_type_conversion(value,y):
+  if y = float:
+    return float(value)
+  elif y = str:
+    return str(value)
+  elif y = int:
+    return int(value)
+  else:
+    print (f"You can't convert {value} into a {y}.")
+
+data_type_conversion()
+
+# Task 5
+
+def grade (*args):
+  average = sum(args) / len (args)
+
+  if average >= 90:
+    print ("A")
+  elif average >= 80 or <= 89:
+    print ("B")
+  elif average >= 70 or <= 79:
+    print ("C")
+  elif average >= 60 or <= 69:
+    print ("D")
+  elif < 60:
+    print("F")
+  else: 
+    print ("Invalid data was provided")
+
+  grade()
+
+# Task 6
+
+  def repeat (string, count):
+    result = ""
+
+    for _ in range (count):
+      result += "string"
+    print (result)
+
+# Task 7
+
+  def student_scores(x, kwargs)
+    
+# Task 8
+
+  def titleize(string):
+
+     little_words = ["a", "on", "an", "the", "of", "and", "is", "in"]
+
+     string_list = string.split()
+
+     for letter in string_list:
+       letter.capitalize()
+    return string.list
+
+
+# Task 9 - will work with mentor on this one
+#  def hangman(secret, guess):
+
+
+
+
+# Task 10 - will work with mentor on this one
+  # def  pig_latin ():
+
+
+
+
+
+
+
+
+
+  
+  
+
+
+
+
+
+
+
+
